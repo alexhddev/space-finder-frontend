0fe3f468796bdf08873ba5ef36a50059ab124cba
barosanuemailtest <barosanu.email.test@gmail.com>
2021-03-25 09:57:06 +0200

    Modal content

---
+++ src/components/spaces/ConfirmModalComponet.tsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import './ConfromModalComponentProps.css';
+
+interface ConfromModalComponentProps {
+    show: boolean,
+    content: string,
+    close: () => void
+}
+
+export class ConfromModalComponent extends Component<ConfromModalComponentProps> {
+
+    render() {
+        if (!this.props.show) {
+            return null
+        } else {
+            return <div className='modal'>
+                <div className='modalContent'>
+                    <h2>You tried to resrve and ...</h2>
+                    <h3 className='modalText'>{this.props.content}</h3>
+                    <button  onClick={()=> this.props.close()} >Ok, close</button>
+                </div>
+            </div>
+        }
+    }
+
+}
