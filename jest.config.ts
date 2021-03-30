b7744dcd2890516d5a66020daf0b5d3a72e32266
barosanuemailtest <barosanu.email.test@gmail.com>
2021-03-30 14:41:54 +0300

    Test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -16,7 +16,9 @@ const config: Config.InitialOptions = {
     testEnvironment: "jsdom",
     collectCoverage: true,
     collectCoverageFrom: [
-        'src/**/*.{ts,tsx}'
+        'src/**/*.{ts,tsx}',
+        '!src/services/**',
+        '!src/react-app-env.d.ts'
     ]
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './components/App';
 
+/* istanbul ignore file */
 ReactDOM.render(
     <App />,
   document.getElementById('root')
