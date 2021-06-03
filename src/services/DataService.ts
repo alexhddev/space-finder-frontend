ceaa22991db6a6662218cb6c17802ad0c655472b
barosanuemailtest <barosanu.email.test@gmail.com>
2021-06-03 10:47:55 +0300

    Uploading public files

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,12 +1,39 @@
 import { ICreateSpaceState } from "../components/spaces/CreateSpace";
 import { Space } from "../model/Model";
+import { S3, config } from 'aws-sdk';
+import { config as appConfig} from './config'
 
+config.update({
+    region: appConfig.REGION
+})
 
 
 export class DataService {
 
+    private s3Client = new S3({
+        region: appConfig.REGION
+    });
+
     public async createSpace(iCreateSpace: ICreateSpaceState){
+        if (iCreateSpace.photo) {
+            const photoUrl = await this.uploadPublicFile(
+                iCreateSpace.photo,
+                appConfig.SPACES_PHOTOS_BUCKET
+            )
+            console.log(photoUrl)
+        }
         return '123'
+    }
+
+    private async uploadPublicFile(file: File, bucket: string){
+        const fileName = file.name;
+        const uploadResult = await new S3({region: appConfig.REGION}).upload({
+            Bucket: bucket,
+            Key: fileName,
+            Body: file,
+            ACL: 'public-read'
+        }).promise();
+        return uploadResult.Location
     }
 
     public async getSpaces(): Promise<Space[]> {
--- src/services/config.ts
+++ src/services/config.ts
@@ -4,5 +4,6 @@ export const config = {
     REGION: 'eu-west-1',
     USER_POOL_ID: 'eu-west-1_gRIUoOdLx',
     APP_CLIENT_ID: '335vma5jmludo42mn2895cu5bh',
-    IDENTITY_POOL_ID: 'eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599'
+    IDENTITY_POOL_ID: 'eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599',
+    SPACES_PHOTOS_BUCKET: 'spaces-photos-02380a0ccf21'
 }
